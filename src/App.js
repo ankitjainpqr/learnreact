c17318beb38f92d4aeb9c67f8a945577d210aaac
Ankit Jain <ankit.jain@indiamart.com>
2018-08-28 18:00:16 +0530

    adding new components and functions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+//Person can be any keyword as per your convenienece
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I am a react app</h1>
+        <p>This is really working</p>
+        <Person name="Ankit" age="28"/>
+        <Person name="Anil" age="50">My Hobbies: Racing</Person>
+        <Person name="Vikas" age="30"/>
       </div>
     );
   }
 }
 
 export default App;
+
+
---
+++ src/Learning/Learning.js
@@ -0,0 +1,11 @@
+//*****************lecture 31 is done*************************
+//In this I learned how to create a new folder inside a react app and how to create a function and how to return html code to App component and how to use this.
+
+//lecture 33 is done
+//to run dynamic content inside the jsx code, put the code in curly braces.
+
+//lecture 35 is done
+//in this lecture, learned how to pass arguments fro App.js to Person.js and how to received these properties in person.js and how to use them.
+
+//lecture 36
+//in this lecture, learned how to show conten b/w two custom html tags.
