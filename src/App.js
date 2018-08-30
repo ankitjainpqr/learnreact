26041f2df49d394a6e9d77a7b6d66277d9e91fc6
Ankit Jain <ankit.jain@indiamart.com>
2018-08-30 18:39:21 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 //Person can be any keyword as per your convenienece
 
 class App extends Component {
+  state = {
+    persons:[
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29},
+      { name: "Stephanie", age: 26 }
+    ]
+  }
+
+  swithNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    this.setState ({
+      persons:[
+        { name: newName, age: 28 },
+        { name: "Manuu", age: 29},
+        { name: "Stephanie", age: 25 }
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a react app</h1>
         <p>This is really working</p>
-        <Person name="Ankit" age="28"/>
-        <Person name="Anil" age="50">My Hobbies: Racing</Person>
-        <Person name="Vikas" age="30"/>
+        <button onClick={this.swithNameHandler.bind(this,'Maxmilian')}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.swithNameHandler.bind(this,'Max!')}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
--- src/Learning/Learning.js
+++ src/Learning/Learning.js
@@ -5,7 +5,29 @@
 //to run dynamic content inside the jsx code, put the code in curly braces.
 
 //lecture 35 is done
-//in this lecture, learned how to pass arguments fro App.js to Person.js and how to received these properties in person.js and how to use them.
+//in this lecture, learned how to pass arguments from App.js to Person.js and how to received these properties in person.js and how to use them.
 
 //lecture 36
 //in this lecture, learned how to show conten b/w two custom html tags.
+
+//lecture 37
+//learned how to initialize state in react and how to pass these states to another function.
+    //write the calling in curly braces.
+
+//lecture 39
+//learned how to make a function inside App component and use it inside App component. The function is called at onClick event and defintion is written in App.js component.
+
+//lecture 41
+//leaned how to update state using setState. Everytime state changes it re-renders either a part of page or complete page.
+//When state changes it means some values have been changes and the places where these values are used will be re-rendered.
+//You can use states in components only and not in functions.
+//states are very important in ReactJs and it must not be used in every component. If it happens then the website will become confustion and hard to maintain.
+//state is reserver keyword.
+
+//lecture 42
+//if a component changes state then it is called container.
+//you dont neeed to change the state everytimme you render dynamic data on your page.
+
+//lecture 43
+//you can define fuction in component and call from different file.
+//how to bind parameters in a function call and definition.
